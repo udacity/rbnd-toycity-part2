bb054ef328facd9ded2d734753e2f28c3005b3d0
Julia Van Cleve <julia@jmv.me>
2016-01-07 11:42:16 -0800

    Clarify % or $ for average discount

--- lib/app.rb
+++ lib/app.rb
@@ -15,7 +15,7 @@ products_hash = JSON.parse(file)
 	# Calculate and print the total number of purchases
   # Calcalate and print the total amount of sales
   # Calculate and print the average price the toy sold for
-  # Calculate and print the average discount based off the average sales price
+  # Calculate and print the average % discount based off the average sales price
 
 # Print "Brands" in ascii art
 
